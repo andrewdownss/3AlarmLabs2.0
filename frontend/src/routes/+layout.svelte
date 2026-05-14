77e575123122a90ceda6c0f816f04d0fae77e089
Andrew Downs <downsa2@g.cofc.edu>
2026-05-14 16:03:36 -0400

    favicon fix

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 
@@ -14,7 +13,6 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 	<!-- Avoid render-blocking: load font CSS as print then promote to all once fetched (Lighthouse). -->
